12f22c255ab8438525883fef5e72d1f86c07eb44
BrentonPhoenix <brentonphoenix@gmail.com>
2022-09-19 20:56:06 -0400

    setting up login component and adding functions, INCOMPLETE. REMEMBER to add axios to project.

--- stytchdemo/src/pages/Login.js
+++ stytchdemo/src/pages/Login.js
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Axios from 'axios'
 export default function Login(){
+    const [email, setEmail] = useState('')
+    const login = () => {
 
+    }
     return(
 
-        <div>
-            <h1>
-                This is the Login component!
-            </h1>
+        <div className="formContainer">
+           <div>
+            <h1>Sign Up</h1>
+           </div>
+           <div className="body">
+                <p> Enter your email to sign up </p>
+                <input type="email" placeholder="demo@email.com" onChange={(e)=> {setEmail(e.target.value)}}/>
+                <button onClick={login}> Login </button>
+           </div>
         </div>
     )
 }
